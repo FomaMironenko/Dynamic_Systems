4c6a85521e7240594e72645662cfff7f3cc2072d
FomaMironenko <fmironenko@reflexion.com>
2021-10-02 16:29:43 +0300

    HW(05.10.21): more accurate way to compute population

--- src/CW_09_28/simulatePopulation.m
+++ src/CW_09_28/simulatePopulation.m
@@ -1,15 +1,26 @@
-function [T, P] = simulatePopulation(model)
-% [T, P] = simulatePopulation(model)
+function [T, P] = simulatePopulation(model, precise)
+% [T, P] = simulatePopulation(model, <precise>)
     assert( checkModel(model, 'P0', 'dt', 'T1', 'c', 'Pm') );
+    params = {model.dt, model.c, model.Pm};
+    if nargin < 2, precise = false; end
     T = 0 : model.dt : model.T1;
     P = zeros(1, numel(T));
     P(1) = model.P0;
-    for i = 2 : numel(P)
-        P(i) = evolution(P(i-1), model.dt, model.c, model.Pm);
+    P(2) = evolution(P(1), params{:});
+    for i = 3 : numel(P)
+        if precise 
+            P(i) = evolutionPlus(P(i-1), P(i-2), params{:});
+        else
+            P(i) = evolution(P(i-1), params{:});
+        end
     end
 end
 
 function p = evolution(prev, dt, c, Pm)
     p = prev + c * dt * (Pm - prev) * prev;
+end
+
+function p = evolutionPlus(prev, preprev, dt, c, Pm)
+    p = preprev + 2 * c * dt * (Pm - prev) * prev;
 end
 
---
+++ src/HW_10_05/population2.m
@@ -0,0 +1,31 @@
+model = struct(...
+    'c', 1.25 * 1e-9, ...
+    'Pm', 25 * 1e6, ...
+    'dt', 10, ...
+    'P0', 3 * 1e6, ...
+    'T1', 200 ...
+);
+
+
+precise = true;
+[~, P1] = simulatePopulation(model, ~precise);
+[T, P2] = simulatePopulation(model, precise);
+Tcont = 0:model.T1;
+N = numel(Tcont);
+realP = arrayfun(@fp, Tcont, ...
+    ones(1, N) * model.Pm, ones(1, N) * model.P0, ones(1, N) * model.c);
+
+hold on;
+plot(T, P1, 'r*');
+plot(T, P2, 'bo');
+plot(Tcont, realP, 'g', 'lineWidth', 2);
+title('data comparison; dt = 10');
+legend('single precision', 'double precision', 'observation')
+xlabel('t, years');
+ylabel('population V');
+
+
+%% analytical population function
+function p = fp(t, Pm, P0, c)
+    p = Pm * P0 / (P0 + (Pm - P0) * exp(-c * Pm * t));
+end
